78954afa9bc63fa5194ab9e3d72d040b8cb941da
jc <jclyler@163.com>
2021-11-22 09:25:46 +0800

    test

--- src/main/java/top/jclyler/helloworld/HelloworldApplication.java
+++ src/main/java/top/jclyler/helloworld/HelloworldApplication.java
@@ -11,6 +11,9 @@ public class HelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
+
+
+
         System.out.println("Hello World!");
     }
 }
